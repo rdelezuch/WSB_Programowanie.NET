2373ea2f3c130b5a4916641b73b61f85eb35cdf2
Robert <gdx147658@student.wsb.gda.pl>
2022-06-25 15:24:50 +0200

    WebApplication

---
+++ WebApplication/Controllers/DaneController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class DaneController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new DaneModel();
+            model.Imie = "Robert";
+            model.Nazwisko = "Deleżuch";
+            return View(model);
+        }
+    }
+}
---
+++ WebApplication/Models/DaneModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class DaneModel
+    {
+        public string? Imie { get; set; }
+        public string? Nazwisko { get; set; }
+    }
+}
